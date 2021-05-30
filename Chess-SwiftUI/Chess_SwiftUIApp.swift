17e66ab0a269e1fe663f8cc08fba7607e9ecf08a
FISHMANSKY <paul96guitar@gmail.com>
2021-05-30 17:21:07 +0200

    Initial Commit

---
+++ Chess-SwiftUI/Chess_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_SwiftUIApp.swift
+//  Chess-SwiftUI
+//
+//  Created by Mac on 30/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Chess_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
